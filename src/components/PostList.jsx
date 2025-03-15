84728a716c1459a45ede41b862f9907bbf41a2a4
Богдан <brazbogdan@gmail.com>
2025-03-15 21:47:19 +0200

    updated commit

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,36 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import PostItem from "./PostItem";
-import MyInput from "./UI/input/MyInput";
-import MyButton from "./UI/button/MyButton";
 
-const PostList = ({ posts, title, updateTitle, removePost, update, toggleComplete }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [newTitle, setNewTitle] = useState(title);
-
-  const handleSaveTitle = () => {
-    updateTitle(newTitle); // Викликає функцію зміни заголовка
-    setIsEditing(false);
-  };
-
+const PostList = ({ posts, removePost, update, toggleComplete }) => {
   return (
     <div className="post-list">
-      <div className="post-list-header">
-        {isEditing ? (
-          <>
-            <MyInput
-              type="text"
-              value={newTitle}
-              onChange={(e) => setNewTitle(e.target.value)}
-            />
-            <MyButton onClick={handleSaveTitle}>Save</MyButton>
-          </>
-        ) : (
-          <h1 onClick={() => setIsEditing(true)} style={{ textAlign: "center", cursor: "pointer" }}>
-            {title}
-          </h1>
-        )}
-      </div>
-      {posts.map((post, index) => (
+    {posts.map((post, index) => (
         <PostItem
           removePost={removePost}
           number={index + 1}
