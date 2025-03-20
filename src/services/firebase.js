97b57b4dd41da1bbdadffa028108b45798f013d1
Богдан <brazbogdan@gmail.com>
2025-03-20 14:45:35 +0200

    updated commit

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHpWK6Qscc1CeKq1YtNlVdOqTf5P-Lgb0",
-  authDomain: "auth-form-bd2c3.firebaseapp.com",
-  projectId: "auth-form-bd2c3",
-  storageBucket: "auth-form-bd2c3.firebasestorage.app",
-  messagingSenderId: "822455842686",
-  appId: "1:822455842686:web:dd6c7690a8e0bab834652f",
-
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
